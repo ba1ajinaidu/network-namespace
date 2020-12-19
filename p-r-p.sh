9ee7c06c1d9f980e6b61ec2e9150429543d9c905
Balajinaidu V <72434180+ba1ajinaidu@users.noreply.github.com>
2020-12-18 23:17:10 -0800

    Add files via upload

---
+++ p-r-p.sh
@@ -0,0 +1,30 @@
+sudo ip netns add box1
+sudo ip netns add router
+sudo ip netns add box2
+
+sudo ip link add veth0 type veth peer name veth1
+sudo ip link set veth0 netns box1
+sudo ip link set veth1 netns router
+
+
+sudo ip link add veth2 type veth peer name veth3
+sudo ip link set veth2 netns box2
+sudo ip link set veth3 netns router
+
+sudo ip netns exec box1 ip addr add 10.0.0.2/24 dev veth0
+sudo ip netns exec router  ip addr add 10.0.0.1/24 dev veth1
+
+sudo ip netns exec box2 ip addr add 10.0.1.2/24 dev veth2
+sudo ip netns exec router ip addr add 10.0.1.1/24 dev veth3
+
+sudo ip netns exec box1 ip link set dev veth0 up
+sudo ip netns exec router ip link set dev veth1 up
+sudo ip netns exec box2 ip link set dev veth2 up
+sudo ip netns exec router ip link set dev veth3 up
+
+
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+sudo echo 1 > /proc/sys/net/netfilter/nf_log_all_netns
+
+sudo ip netns exec box1 ip route add default via 10.0.0.1
+sudo ip netns exec box2 ip route add default via 10.0.1.1
