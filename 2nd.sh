c288862c98def872a509ae1401b0c9a724abff78
Balajinaidu V <72434180+ba1ajinaidu@users.noreply.github.com>
2020-12-19 07:52:05 -0800

    Add files via upload

---
+++ 2nd.sh
@@ -0,0 +1,38 @@
+sudo ip netns add b1
+sudo ip netns add b2
+sudo ip netns add r1
+sudo ip netns add r2
+
+sudo ip link add veth0 type veth peer name veth1
+sudo ip link set veth0 netns b1
+sudo ip link set veth1 netns r1
+
+sudo ip link add veth2 type veth peer name veth3
+sudo ip link set veth2 netns r1
+sudo ip link set veth3 netns r2
+
+sudo ip link add veth4 type veth peer name veth5
+sudo ip link set veth4 netns r2
+sudo ip link set veth5 netns b2
+
+sudo ip netns exec b1 ip addr add 10.0.0.2/24 dev veth0
+sudo ip netns exec r1 ip addr add 10.0.0.1/24 dev veth1
+
+sudo ip netns exec r1 ip addr add 10.0.1.1/24 dev veth2
+sudo ip netns exec r2 ip addr add 10.0.1.2/24 dev veth3
+
+sudo ip netns exec r2 ip addr add 10.1.1.1/24 dev veth4
+sudo ip netns exec b2 ip addr add 10.1.1.2/24 dev veth5
+
+sudo ip netns exec b1 ip link set dev veth0 up
+sudo ip netns exec r1 ip link set dev veth1 up
+sudo ip netns exec r1 ip link set dev veth2 up
+sudo ip netns exec r2 ip link set dev veth3 up
+sudo ip netns exec r2 ip link set dev veth4 up
+sudo ip netns exec b2 ip link set dev veth5 up
+
+sudo ip netns exec r1 sysctl -w net.ipv4.conf.all.forwarding=1
+sudo ip netns exec r2 sysctl -w net.ipv4.conf.all.forwarding=1
+
+sudo ip netns exec b1 ip route add default via 10.0.0.1
+sudo ip netns exec b2 ip route add default via 10.1.1.1
