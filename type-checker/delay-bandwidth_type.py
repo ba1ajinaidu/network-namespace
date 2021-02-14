cf432256f712b3f6df1587d24572b163f09c6e1e
Balajinaidu V <72434180+ba1ajinaidu@users.noreply.github.com>
2021-02-14 14:10:00 +0530

    new version

--- type-checker/delay-bandwidth_type.py
+++ type-checker/delay-bandwidth_type.py
@@ -4,7 +4,8 @@ def split_string(string):
     #r = re.sub(r'(?<=\d)(?=\D)|(?<=\D)(?=\d)', r' ', string).split()
     r = re.split(r'((\[0-9].[0-9])|([a-zA-Z]+))',string)
     #r = re.
-    return r
+    res = None if len(r)==1 else r
+    return res
 
 def delay_type():
     test='85.76ms'
@@ -12,31 +13,40 @@ def delay_type():
     delay =['s','sec','secs','ms','msec','msecs','us','usec','usecs']
     
     print(result)
-    if(result[0].replace('.','',1).isdigit()):
-        if(result[1] in delay):
-            print("continue normally")
+    
+    if(result!= None):
+        if(result[0].replace('.','',1).isdigit()):
+            if(result[1] in delay):
+                print("continue normally")
+            
+            else:
+                print("please provide a proper unit")
             
         else:
-            print("please provide a proper unit")
+            print("usage 85ms")
             
     else:
-        print("usage 85ms")
+            print("no unit was passed along with delay")
         
 def bandwidth_type():
-    test1='85mbit'
+    test1='85'
     result1=split_string(test1)
-    bandwidth =['bit','kbit','kibit','mbit','mibit','gbit','gibit','tbit','tibit','bps','kbps','kibps','mbps','mibps','gbps','gibps','tbps','tibps']
+    bandwidth =['bit','kbit','kibit','mbit','mebit','gbit','gibit','tbit','tebit','bps','kbps','kibps','mbps','mebps','gbps','gibps','tbps','tebps']
     
     print(result1)
-    if(result1[0].replace('.','',1).isdigit()):
-        if(result1[1] in bandwidth):
-            print("continue normally")
+    if(result1 != None):
+        if(result1[0].replace('.','',1).isdigit()):
+            if(result1[1] in bandwidth):
+                print("continue normally")
+            
+            else:
+                print("Please provide a proper unit(type)")
             
         else:
-            print("Please provide a proper unit(type)")
+            print("usage 85mbit")
             
     else:
-        print("usage 85mbit")
+        print("no unit was passed along with bandwidth")
 
 delay_type()
 bandwidth_type()
